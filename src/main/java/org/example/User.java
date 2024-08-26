7c81f61d823dddea8b061467af2ded16ff442c10
asavershin <avershinrussian@gmail.com>
2024-08-26 17:11:39 +0300

    Pojo to pdf converting

---
+++ src/main/java/org/example/User.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class User {
+
+  private String name;
+  private int age;
+  private String email;
+
+}
+
